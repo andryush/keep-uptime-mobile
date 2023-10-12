22c73f80d679a2d922136d4188b188b5928c55c7
andrey.arakelyan <andrey.arakelyan@sentium-consulting.com>
2023-10-13 03:38:41 +0400

    feat: add validations to verification screen

--- app/(public)/verification.tsx
+++ app/(public)/verification.tsx
@@ -5,13 +5,25 @@ import {
   Button,
   ButtonText,
   Image,
+  Text,
 } from '@gluestack-ui/themed';
 import { router } from 'expo-router';
+import { Controller, useForm } from 'react-hook-form';
 
 import BlurredContainer from '../../components/BlurredContainer';
 import KeyboardAvoidingWrapper from '../../components/KeyboardAvoidingWrapper';
 
 const Verification = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleVerify = () => router.push('/new-password');
+
+  console.log(errors);
+
   return (
     <KeyboardAvoidingWrapper>
       <Box width={'100%'} height={'100%'} bgColor={'$black'}>
@@ -24,17 +36,46 @@ const Verification = () => {
         <Box>
           <BlurredContainer header={'Verification'} marginTop={0}>
             <Input variant="outline" size="xl" bgColor={'$white'}>
-              <InputField
-                type={'text'}
-                placeholder={'Verification Code'}
-                autoCapitalize={'none'}
+              <Controller
+                name={'verification'}
+                control={control}
+                rules={{
+                  required: {
+                    value: true,
+                    message: 'This field is required',
+                  },
+                  minLength: {
+                    value: 6,
+                    message: 'Verification code should be 6 digits',
+                  },
+                  maxLength: {
+                    value: 6,
+                    message: 'Verification code should be 6 digits',
+                  },
+                }}
+                defaultValue={''}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <InputField
+                    type={'text'}
+                    placeholder={'Verification Code'}
+                    autoCapitalize={'none'}
+                    onChangeText={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                  />
+                )}
               />
             </Input>
+            {errors?.verification && (
+              <Text color={'red'}>
+                {errors?.verification?.message?.toString()}
+              </Text>
+            )}
             <Button
               size="xl"
               variant="solid"
               action="primary"
-              onPress={() => router.push('/new-password')}>
+              onPress={handleSubmit(handleVerify)}>
               <ButtonText>Verify</ButtonText>
             </Button>
             <Box>
