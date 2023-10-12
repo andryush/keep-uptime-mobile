63bf88ef184c55e9a467ffc903d92f5c1142a75e
andrey.arakelyan <andrey.arakelyan@sentium-consulting.com>
2023-10-12 12:10:41 +0400

    feat: add validation to sign in page

--- app/(public)/sign-in.tsx
+++ app/(public)/sign-in.tsx
@@ -6,14 +6,26 @@ import {
   ButtonText,
   HStack,
   Image,
+  Text,
 } from '@gluestack-ui/themed';
 import { router } from 'expo-router';
+import { useForm, Controller } from 'react-hook-form';
 
 import Logo from '../../assets/svgs/logo.svg';
 import BlurredContainer from '../../components/BlurredContainer';
 import KeyboardAvoidingWrapper from '../../components/KeyboardAvoidingWrapper';
 
 const SignIn = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleLogin = () => {
+    router.push('/home');
+  };
+
   return (
     <KeyboardAvoidingWrapper>
       <Box width={'100%'} height={'100%'} bgColor={'$black'}>
@@ -34,16 +46,69 @@ const SignIn = () => {
         </Box>
         <BlurredContainer header={'Sign In'}>
           <Input variant="outline" size="xl" bgColor={'$white'}>
-            <InputField
-              type={'text'}
-              placeholder={'Email'}
-              autoCapitalize={'none'}
+            <Controller
+              name={'email'}
+              control={control}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'This field is required',
+                },
+                pattern: {
+                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+                  message: 'Please enter correct email',
+                },
+              }}
+              defaultValue={''}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <InputField
+                  onChangeText={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  type={'text'}
+                  placeholder={'Email'}
+                  autoCapitalize={'none'}
+                />
+              )}
             />
           </Input>
+          {errors?.email && (
+            <Text color={'red'}>{errors?.email?.message?.toString()}</Text>
+          )}
           <Input variant="outline" size="xl" bgColor={'$white'}>
-            <InputField type={'password'} placeholder={'Password'} />
+            <Controller
+              defaultValue={''}
+              name={'password'}
+              control={control}
+              rules={{
+                required: {
+                  value: true,
+                  message: 'This field is required',
+                },
+                minLength: {
+                  value: 8,
+                  message: 'Password must contain at least 8 characters',
+                },
+              }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <InputField
+                  type={'password'}
+                  placeholder={'Password'}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+            />
           </Input>
-          <Button size="xl" variant="solid" action="primary" onPress={() => {}}>
+          {errors?.password && (
+            <Text color={'red'}>{errors?.password?.message?.toString()}</Text>
+          )}
+          <Button
+            size="xl"
+            variant="solid"
+            action="primary"
+            onPress={handleSubmit(handleLogin)}>
             <ButtonText>Sign In</ButtonText>
           </Button>
           <Box>
